514f18dfeab93d6ab15769513682433946f64e46
chewam <gary@chewam.com>
2021-11-01 20:26:04 +0100

    chore(database): Clean connection string

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,17 +3,21 @@ import { Pool } from "pg";
 const {
   NODE_ENV,
   POSTGRES_PORT,
+  POSTGRES_HOST,
   POSTGRES_USERNAME,
   POSTGRES_PASSWORD,
   POSTGRES_DATABASE,
-  POSTGRES_HOST,
 } = process.env;
+
+const database = POSTGRES_DATABASE;
+const host = `${POSTGRES_HOST}:${POSTGRES_PORT}`;
+const user = `${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}`;
 
 const pool = new Pool({
   max: 4,
   idleTimeoutMillis: 30000,
   ssl: NODE_ENV === "production",
-  connectionString: `postgres://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`,
+  connectionString: `postgres://${user}@${host}/${database}`,
 });
 
 const query = async (sql: string, values: string[] = []) => {
