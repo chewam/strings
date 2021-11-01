be36993a12f8ea07d8d63574da556b2a5a0705c1
chewam <gary@chewam.com>
2021-11-01 20:21:12 +0100

    chore(database): Use NODE_ENV to toggle ssl connection

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const {
-  PRODUCTION,
+  NODE_ENV,
   POSTGRES_PORT,
   POSTGRES_USERNAME,
   POSTGRES_PASSWORD,
@@ -11,13 +11,13 @@ const {
 
 const pool = new Pool({
   max: 4,
-  ssl: !!PRODUCTION,
   idleTimeoutMillis: 30000,
+  ssl: NODE_ENV === "production",
   connectionString: `postgres://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`,
 });
 
 const query = async (sql: string, values: string[] = []) => {
-  console.log("QUERY:", sql, values);
+  console.log("QUERY:", sql, values, NODE_ENV);
   const client = await pool.connect();
   const { rows } = await client.query(sql, values);
   client.release();
